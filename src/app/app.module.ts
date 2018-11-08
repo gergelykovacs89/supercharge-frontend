811b3fadba0edcf4eb4bb28a5686219692192bf4
Gergely Kovacs <gergelyk89@gmail.com>
2018-11-08 09:45:13 +0100

    components setup, basic header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { IndexComponent } from './home/index/index.component';
+import { GameComponent } from './home/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    IndexComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
