544bb70a60a09b8a3edeeea66ec528da0ecac97c
Gergely Kovacs <gergelyk89@gmail.com>
2018-11-08 11:57:04 +0100

    basic game logic implemented

--- src/app/home/game/game.component.ts
+++ src/app/home/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GameService} from '../../service/game.service';
 import {GameModel} from '../../model/game.model';
+import {CardModel} from '../../model/card.model';
 
 @Component({
   selector: 'app-game',
@@ -9,11 +10,44 @@ import {GameModel} from '../../model/game.model';
 })
 export class GameComponent implements OnInit {
   public game: GameModel;
+  firstClickCard: CardModel;
+  secondClickCard: CardModel;
 
   constructor(private gameService: GameService) { }
 
   ngOnInit() {
     this.game = this.gameService.setupGame();
+  }
+
+  checkGameStatus(): boolean {
+    if (this.secondClickCard.img === this.firstClickCard.img) {
+      this.secondClickCard.found = true;
+      this.firstClickCard.found = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  onClick(card: CardModel) {
+    this.game.score++;
+    console.log(this.game.score);
+    if (this.game.score % 2 !== 0) {
+      this.firstClickCard = card;
+      console.log(card.img + ' first card');
+      this.firstClickCard.flipCard();
+    } else if (this.game.score % 2 === 0) {
+      this.secondClickCard = card;
+      console.log(card.img + ' second card');
+      this.secondClickCard.flipCard();
+      const isFound = this.checkGameStatus();
+      console.log(isFound);
+      if (!isFound) {
+        setTimeout(() => this.firstClickCard.flipCard(), 1000);
+        setTimeout(() => this.secondClickCard.flipCard(), 1000);
+      }
+    }
   }
 
 }
--- src/app/model/card.model.ts
+++ src/app/model/card.model.ts
@@ -3,12 +3,14 @@ export class CardModel {
   img: string;
   backImg: string;
   flipped: boolean;
+  found: boolean;
 
   constructor(id: number, img: string) {
     this.id = id;
     this.img = img;
     this.backImg = 'assets/cards/supercharge.png';
     this.flipped = false;
+    this.found = false;
   }
 
   flipCard() {
